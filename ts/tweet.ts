5e23498127d7548bc459d57b24e72e9967de4578
hwyam <hwyam@uci.edu>
2025-10-29 22:58:44 -0700

    Update tweet.ts

--- ts/tweet.ts
+++ ts/tweet.ts
@@ -7,6 +7,18 @@ class Tweet {
 		this.time = new Date(tweet_time);//, "ddd MMM D HH:mm:ss Z YYYY"
 	}
 
+    //helper function
+    private static strip(text:string): string{
+        let s = text.replace(/https?:\/\/\S+/gi, "")
+                    .replace(/#runkeeper/gi,"")
+                    .replace(/with\s+@runkeeper\.?/gi, "")
+                    .replace(/check it out!?/gi, "")
+                    .replace(/just\s+(completed | posted)\s+a/gi, "")
+                    .replace(/\s+/g, " ")
+                    .trim();
+        return s;
+    }
+
 	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
     get source():string {
         //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
@@ -14,15 +26,19 @@ class Tweet {
         if (t.startsWith("just completed a") || t.startsWith("just finished a") || t.startsWith("just posted a")){
             return "complete";
         }
-        return "live event";
-        return "achievement"
+        if (t.includes("currently") || t.includes("right now")){
+            return "live event";
+        }
+        if (t.includes("achieved") || t.includes("record")){
+            return "achievement"
+        }
         return "mischellaneous";
     }
 
     //returns a boolean, whether the text includes any content written by the person tweeting.
     get written():boolean {
         //TODO: identify whether the tweet is written
-        return false;
+        return Tweet.strip(this.text).length > 0;
     }
 
     get writtenText():string {
@@ -30,7 +46,7 @@ class Tweet {
             return "";
         }
         //TODO: parse the written text from the tweet
-        return "";
+        return Tweet.strip(this.text);
     }
 
     get activityType():string {
@@ -38,7 +54,28 @@ class Tweet {
             return "unknown";
         }
         //TODO: parse the activity type from the text of the tweet
-        return "";
+        const lowerText = this.text.toLowerCase();
+        const content = lowerText.match(/(\d+(?:\.\d+)?)\s*(km|mi)\s+([a-z]+)/);
+
+        if (!content) {
+            return "unknown";
+        }
+
+        let activity = content[3];
+        if (activity.includes("running")) {
+            return "run";
+        }
+        if (activity == "walking") {
+            return "walk";
+        }
+        if (activity == "swimming") {
+            return "swim";
+        }
+        if (activity.includes("biking") || (activity.includes("cycling")) ){
+            return "bike";
+        }
+
+        return activity.charAt(0).toUpperCase() + activity.substring(1);
     }
 
     get distance():number {
@@ -46,11 +83,43 @@ class Tweet {
             return 0;
         }
         //TODO: prase the distance from the text of the tweet
-        return 0;
+        const lowerText = this.text.toLowerCase();
+        const content = lowerText.match(/(\d+(\.\d+)?)\s*(km|mi)/);
+
+        if(!content) {
+            return 0;
+        }
+
+        const value = parseFloat(content[1]);
+        const unit = content[3];
+
+        let mile = value;
+        if (unit == "km"){
+            mile = value / 1.609;
+        }
+        return +mile.toFixed(2);
     }
 
     getHTMLTableRow(rowNumber:number):string {
         //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
-        return "<tr></tr>";
+        const urlContent = this.text.match(/https?:\/\/\S+/i);
+        
+        let url;
+        if (urlContent){
+            url = urlContent[0];
+        } else{
+            url = "#";
+        }
+
+        const safeText = this.text
+                             .replace(/&/g, "&amp;")
+                             .replace(/</g, "&lt;")
+                             .replace(/>/g, "&gt;");
+                             
+        return `<tr>
+                    <td>${rowNumber}</td>
+                    <td>${this.activityType}</td>
+                    <td><a href = "${url}" target= "_blank">${safeText}</a></td>
+                </tr>`;
     }
 }
